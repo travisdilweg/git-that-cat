4b0fe8e2badc654f8922c2a019be688ed7179812
Travis Dilweg <travis.dilweg@gmail.com>
2015-11-25 02:17:35 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GitThatCat::Application.config.secret_key_base = '0130dbbde6cd4255fa93780e8529727e18dcafeb8a0670b08b6e3044c61c8b8aef216a1f23dd9f8aaccf680478b01dc794286255f8197b56ea32ee130d91d839'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+GitThatCat::Application.config.session_store :cookie_store, key: '_git_that_cat_session'
